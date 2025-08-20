eb5cf00bbe2440d6a9ac43692e9fc253d8244c05
shashank-wadi <shashankwadi@gmail.com>
2025-08-20 14:21:02 +0530

    sss

--- backend/api/index.js
+++
@@ -1,6 +0,0 @@
-const app = require("./server"); // import express app from server.js
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-  console.log(`🚀 Server running on port ${port}`);
-});
